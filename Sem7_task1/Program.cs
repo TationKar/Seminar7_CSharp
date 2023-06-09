bb4df54346b50fbe499567ab1c88f5ea46f2c164
Timur Karimov <tationkar@gmail.com>
2023-06-09 21:49:55 +0300

    sec

--- Sem7_task1/Program.cs
+++ Sem7_task1/Program.cs
@@ -1,2 +1,52 @@
-﻿// S
-Console.Clear();
+﻿using System;
+using static System.Console;
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+
+int[,] GetSqr(int[,] array1)
+{
+    for (int i = 0; i < array1.GetLength(0); i++){
+        for (int j = 0; j < array1.GetLength(1); j++)
+{
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array1[i, j] *= array1[i, j];
+
+            }
+
+        }
+    }
+    return array1;
+}
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+WriteLine("Введите число n: ");
+int n = int.Parse(ReadLine());
+WriteLine("Введите число m: ");
+int m = int.Parse(ReadLine());
+int[,] array = GetArray(m, n);
+PrintArray(array);
+WriteLine();
+int[,] array1 = GetSqr(array);
+PrintArray(array1);
--- Sem7_task2/Program.cs
+++ Sem7_task2/Program.cs
@@ -1,2 +1,50 @@
-﻿// S
-Console.Clear();
+﻿using System;
+using static System.Console;
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(100);
+        }
+    }
+    return result;
+}
+
+int GetSum(int[,] array1)
+{
+    int sum = 0;
+   int min = array1.GetLength(0) < array1.GetLength(1) ? array1.GetLength(0) : array1.GetLength(1);
+   
+    for (int i = 0; i < min; i++){
+                
+                sum += array1[i, i];
+            }
+        
+    
+    return sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+WriteLine("Введите число n: ");
+int n = int.Parse(ReadLine());
+WriteLine("Введите число m: ");
+int m = int.Parse(ReadLine());
+int[,] array = GetArray(m, n);
+PrintArray(array);
+WriteLine();
+WriteLine($"Сумма значений элементов главной  диагонали = {GetSum(array)}");
